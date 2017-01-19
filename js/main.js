576eff1a98e541f9616668ada5d9795025808dfa
SerSmoothie <SerSmoothie>
2017-01-19 21:07:05 +0100

    updated layout, script and content

--- js/main.js
+++ js/main.js
@@ -12,58 +12,129 @@ $(document).ready(function() {
 
 	$frontEndClick = $portfolioNavbar2.find("#frontEndClick");
 	$backEndClick = $portfolioNavbar2.find("#backEndClick");
+	$slideIndicator = $portfolioNavbar2.find(".butt");
+	$slideIndicator1 = $frontEndClick.find(".butt");
+	$slideIndicator2 = $backEndClick.find(".butt");
 
 	$sliderDiv = $portfolioBody.find(".slider");
-	$displayUl = $sliderDiv.find("ul");
+	$displayUl = $sliderDiv.find(".slide");
 	$displayLi = $displayUl.find("li");
-	$display = $displayLi.find(".display")
-
+	$display = $displayLi.find(".show");
+	$displaySpan = $display.find("span");
+    
+    $frontend = $sliderDiv.find("#frontend");
+    $backend = $sliderDiv.find("#backend");
+	$exampleImg = $(".exampleImg");
+	
+	$modal = $("body").find("#myModal");
+	$modalHeader = $modal.find(".modal-header")
+	$modalContent = $modal.find(".modal-content");
+	$modalBody = $modalContent.find(".modal-body");
+	$modalFooter = $modalContent.find(".modal-footer");
+	
 //Variables
 	slideSpeed = 500;
-	screenWidth = $(window).width() * 2;
-	screenHeight = $(window).height();
+	screenWidth = 0;
+	screenHeight = 0;
+	bodyHeight = 0;
 	currentPage = $portfolioBody;
 
+//Size function
+	var calculateSize = function() {
+		
+		//screenwidth
+		screenWidth = $(window).width();
+		screenHeight = $(window).height();
+		bodyHeight = $(document).height();
+		
+		//body class
+		if ($resumeBody.height() < $(window).height()) {
+			setHeight = $(window).height() - ($resumeBody.height() - $resumeBody.find(".body").height());
+			$resumeBody.find(".body").height(setHeight);
+		}
+		
+		if ($aboutMeBody.height() < $(window).height()) {
+			setHeight = $(window).height() - ($aboutMeBody.height() - $aboutMeBody.find(".body").height());
+			$aboutMeBody.find(".body").height(setHeight);
+		}
+		
+		
+		//slideindicator
+		$slideIndicator1.css("left","0px");
+		$slideIndicator2.css("left","-" + $slideIndicator2.width() + "px");
+		
+		//height
+		if ($frontend.height() > $backend.height()) {
+            $backend.height($frontend.height());
+        } else {
+            $frontend.height($backend.height());
+        };
+		
+		//exampleImg
+		//$displaySpan.width() = $display.width();
+		
+		$exampleImg.each(function() {
+			thisAspectRatio = $(this).width() / $(this).height();
+		});
+	};
+	
 //functions
 	
 	var slideRight = function() {
 		if ($displayUl.css("margin-left") === "0px") {
-			$displayUl.animate({"margin-left": "-=" + screenWidth}, slideSpeed, function() {
-				console.log("sliding right");
+			$displayUl.velocity({"margin-left": "-=" + screenWidth}, slideSpeed, function() {
 			});
+			$slideIndicator.velocity({"margin-left": "+=" + (screenWidth / 2) + "px"}, slideSpeed);
 		};
 	};
 
 	var slideLeft = function() {
 		if ($displayUl.css("margin-left") === -screenWidth + "px") {
-			$displayUl.animate({"margin-left": "+=" + screenWidth}, slideSpeed, function() {
-				console.log("sliding left");
+			$displayUl.velocity({"margin-left": "+=" + screenWidth}, slideSpeed, function() {
 			});
+			$slideIndicator.velocity({"margin-left": "-=" + (screenWidth / 2) + "px"}, slideSpeed);
 		};
 	};
 
 	var slideUp = function(newPage) {
-		currentPage.animate({"margin-top": "-=" + screenHeight}, slideSpeed, function() {
+		currentPage.velocity({"margin-top": "-=" + bodyHeight}, slideSpeed, function() {
 			$sites.addClass("hidden");
-			newPage.css("margin-top", screenHeight);
+			newPage.css("margin-top", bodyHeight);
 			newPage.removeClass("hidden");
-			newPage.animate({"margin-top": "-=" + screenHeight}, slideSpeed, function() {
+			newPage.animate({"margin-top": "-=" + bodyHeight}, slideSpeed, function() {
 				currentPage = newPage;
 			});
 		});
 	};
-
-	console.log(screenWidth + "px");
+	
 	$frontEndClick.click(function() {
 		slideLeft();
 	});
 	$backEndClick.click(function() {
 		slideRight();
 	});
-
-
-
-	$(".btn").click(function() {
+	
+	calculateSize();
+	
+	$(".show").on("click", function() {
+		if ($modal.hasClass("hidden")) {
+			$modal.removeClass("hidden");
+		};
+		$modal.modal("toggle");
+		$modalHeader.empty();
+		$modalBody.empty();
+		$modalFooter.empty();
+		
+		$(this).find(".modal-title").clone().appendTo($modalHeader);
+		$(this).find(".modal-description").clone().appendTo($modalBody);
+		$(this).find(".modal-link").clone().appendTo($modalBody);
+		$modalBody.find(".modal-description").removeClass("hidden");
+		$modalBody.find(".modal-link").removeClass("hidden");
+		$(this).find("img").clone().appendTo($modalBody);
+		$(this).find("ul").clone().appendTo($modalFooter);
+	});
+	
+	$(".btn").on("click", function() {
 		var $id = $(this).attr("value");
 		if ($id === "portfolioBtn" && currentPage !== $portfolioBody) {
 			slideUp($portfolioBody);
@@ -73,4 +144,9 @@ $(document).ready(function() {
 			slideUp($aboutMeBody);
 		};
 	});
-}); 
+	
+	$(window).resize(calculateSize).trigger("resize");
+	
+	$resumeBody.addClass("hidden");
+	$aboutMeBody.addClass("hidden");
+});
